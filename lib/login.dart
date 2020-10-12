6d628e71c4f7c151e1e6e9d5937d346e3fab486a
Lucas Anjos <lucasoneplanet@gmail.com>
2020-10-12 16:22:26 -0300

    first commit

---
+++ lib/login.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('LOGIN'),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15.0),
+            child: TextField(
+              decoration: InputDecoration(labelText: 'Email'),
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15.0),
+            child: TextField(
+              decoration: InputDecoration(labelText: 'Senha'),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
